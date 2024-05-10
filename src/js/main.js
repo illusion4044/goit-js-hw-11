84695ab34bc927c56911738fb55611f773fb4fa8
Andrii <bugaychuk9aa@gmail.com>
2024-05-10 19:49:33 +0300

    general update

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,80 @@
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import { createGalleryItemMarkup } from './render-functions.js';
 import { fetchPhotosByQuery } from './pixabay-api.js';
 
-import 'izitoast/dist/css/iziToast.min.css';
 
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryEl = document.querySelector('.js-gallery');
 const loaderEl = document.querySelector('.js-loader');
 
+let lightbox;
+
 function onSearchFormSubmit(event) {
     event.preventDefault();
     const searchQuery = event.target.elements.searchKeyword.value.trim();
-  
+
     if (searchQuery === '') {
-      galleryEl.innerHTML = '';
-      event.target.reset();
-      iziToast.show({
-        message: 'Input field can not be empty',
-        position: 'topRight',
-        timeout: 2000,
-        color: 'red',
-      });
-  
-      return;
-    }
-  
-    galleryEl.innerHTML = '';
-    loaderEl.classList.remove('is-hidden');
-  
-    fetchPhotosByQuery(searchQuery)
-      .then(imagesData => {
-        if (imagesData.total === 0) {
-          iziToast.show({
-            message: 'Sorry, there are no images for this query',
+        galleryEl.innerHTML = '';
+        event.target.reset();
+        iziToast.show({
+            message: 'Input field cannot be empty',
             position: 'topRight',
             timeout: 2000,
             color: 'red',
-          });
-        }
-  
-        galleryEl.innerHTML = createGalleryItemMarkup(imagesData.results);
-      })
-      .catch(error => console.log(error))
-      .finally(() => {
-        event.target.reset();
-        loaderEl.classList.add('is-hidden');
-      });
-  }
-  
-  searchFormEl.addEventListener('submit', onSearchFormSubmit);
+        });
+        return;
+    }
+
+    galleryEl.innerHTML = '';
+    loaderEl.classList.remove('is-hidden');
+
+    fetchPhotosByQuery(searchQuery)
+        .then(imagesData => {
+            console.log('Received data:', imagesData); 
+
+            
+            if (!imagesData.hits || imagesData.hits.length === 0) {
+                iziToast.show({
+                    message: "Sorry, there are no images matching your search query. Please try again!",
+                    position: 'topRight',
+                    timeout: 2000,
+                    color: 'red',
+                });
+
+                galleryEl.innerHTML = '';
+                return;
+            }
+
+            
+            galleryEl.innerHTML = createGalleryItemMarkup(imagesData.hits);
+
+            if (lightbox) {
+              lightbox.destroy(); // Знищуємо попередній екземпляр
+            }
+            lightbox = new SimpleLightbox('.js-gallery a', {
+              captionDelay: 250,
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching photos:', error);
+            iziToast.show({
+                message: 'An error occurred while fetching photos',
+                position: 'topRight',
+                timeout: 2000,
+                color: 'red',
+            });
+        })
+        .finally(() => {
+            event.target.reset();
+            loaderEl.classList.add('is-hidden');
+        });
+}
+
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,19 @@
-const API_KEY = '813609-98f1a5b8a61f7cd1a2ca10379';
+const API_KEY = '43813609-98f1a5b8a61f7cd1a2ca10379'; 
 const BASE_URL = 'https://pixabay.com/api/';
 
 export const fetchPhotosByQuery = (query = 'cat') => {
-  const searchParams = new URLSearchParams({
-    key: API_KEY, 
-    q: query,
-    orientation: 'horizontal', 
-  });
+    const searchParams = new URLSearchParams({
+        key: API_KEY,
+        q: query,
+        orientation: 'horizontal',
+    });
 
-  return fetch(`${BASE_URL}?${searchParams.toString()}`).then(response => {
-    if (!response.ok) {
-      throw new Error(`Error ${response.status}: ${response.statusText}`);
-    }
+    return fetch(`${BASE_URL}?${searchParams.toString()}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error ${response.status}: ${response.statusText}`);
+            }
 
-    return response.json();
-  });
+            return response.json();
+        });
 };
