aefa76d8a64816ea284b50cd5c73cae795885734
illusion4044 <117983189+illusion4044@users.noreply.github.com>
2024-05-10 13:08:43 +0000

    Deploying to gh-pages from @ illusion4044/goit-js-hw-11@7fa08204ef9d1b38249cce891d1cf84a76317bf1 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,20 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import"./assets/vendor-86291ea8.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const i=r=>r.map(({urls:o,alt_description:s,likes:n})=>`
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src="${o.regular}" alt="${s}" class="gallery-img img-fluid" />
+            <div class="card-body">
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">
+                    View
+                  </button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">
+                    Edit
+                  </button>
+                </div>
+                <small class="text-body-secondary">Likes: ${n}</small>
+              </div>
+            </div>
+          </div>
+        </div>`).join(""),a="813609-98f1a5b8a61f7cd1a2ca10379",l="https://pixabay.com/api",u=(r="cat")=>{const o=new URLSearchParams({query:r,client_id:a,orientation:"horizontal"});return fetch(`${l}?${o}`).then(s=>{if(!s.ok)throw new Error(s.statusText);return s.json()})};document.querySelector(".js-search-form");document.querySelector(".js-gallery");document.querySelector(".js-loader");u().then(r=>{gelleryEL.innerHTML=i(imagesData.results)}).catch(r=>console.log(r));
 //# sourceMappingURL=commonHelpers.js.map
